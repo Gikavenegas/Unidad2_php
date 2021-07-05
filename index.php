c8ca2595d2dcbd50833242ec6fd2f946886733fb
Angie <gikavenegas@gmail.com>
2021-07-04 22:19:47 -0300

    Subo el ejercicio de php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> Estos son mis datos: </h1>
+</n>
+
+<?php
+
+echo "<b>Nombre:</b> Angie Katherine";
+echo "<br>";
+echo "<b>Apellido:</b> Venegas Cedeño"; 
+echo "<br>";
+echo "<b>Edad:</b> 31"; 
+echo "<br>";
+echo "<b>Hobbie:</b> programar, dibujar, armar rompecabezas, hacer sudokus..."; 
+echo "<br>";
+echo "<b>Editor de código preferido:</b> Visual Studio Code";
+echo "<br>";
+echo "<b>Sistema operativo que utiliza:</b> ";
+echo PHP_OS;
+
+?>
+    
+</body>
+</html>
